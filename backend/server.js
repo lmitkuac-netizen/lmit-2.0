56ae904118028b71e59269e95d68b51ad66cb9b0
Guu3t <nathanlablue68@gmail.com>
2026-05-22 02:30:01 +0700

    connect db

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const pool = require("./db");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -9,6 +10,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend running");
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("DB Error");
+  }
 });
 
 const PORT = process.env.PORT || 10000;
